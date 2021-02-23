29c36e9577f6ff79070925dd71373ad215b643e1
tmuhader <58604675+tmuhader@users.noreply.github.com>
2021-02-23 11:58:48 +0400

    Configure and Test logging inside container
    in emaginer-app, we are logging to the console transport when NODE_ENV === 'development', so we can see the container logs in WS Docker console or by typing the command:  docker container logs emaginer-dev-cn
    add logging to index route to test the endpoint access logging
    
     Changes to be committed:
    	modified:   docker/DockerfileDev
    	new file:   docker/WS-Docker-configuration.png
    	modified:   docker/containerizing-manual.md
    	modified:   src/routers/index.js

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,10 +1,16 @@
 // Main router entry point, sets up all route modules  for all resources
 import express from 'express';
 import userRouter from './usersRouter.js';
+import createLogger from "../utils/logger.js";
 const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  const logger = createLogger('server.endpoint.get.index');
+  logger.info({
+    message: '************enter the index route*************',
+    remoteAddress: req.connection.remoteAddress,
+  });
   res.render('index', { title: 'Express' });
 });
 
